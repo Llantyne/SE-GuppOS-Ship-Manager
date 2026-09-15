2a6f51bb34fe9781263523f440576fec498b04ce
Llantyne <cfb426@gmail.com>
2026-09-14 22:20:48 -0400

    still screwing around

--- Connectors.cs
+++ Connectors.cs
@@ -1,3 +1,29 @@
+using KeenSoftwareHouse.Library.Extensions;
+using Sandbox.Game.Entities;
+using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.SessionComponents;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
 namespace IngameScript
 {
     public partial class Program : MyGridProgram
